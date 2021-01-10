c20ca59bd3c4395e0cada0122d58ae0a7fe8a50c
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2021-01-09 16:22:57 -0800

    Added original table data

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,9 @@ var tableData = data;
 // Select the button
 var button = d3.select("#filter-btn");
 
-// Select the form
+// Select the forms
 var form = d3.select("#datetime");
+
 
 // Select table body
 var tbody = d3.select("tbody");
@@ -21,12 +22,14 @@ function runEnter () {
     // Prevent page from refreshing
     d3.event.preventDefault();
     
-    // Select input element and get raw HTML
+    // Select input elements and get raw HTML
     var inputElement = d3.select("#datetime");
+    var inputElement2 = d3.select("#cityinput");
     
-    // Get the value property of the input element
+    // Get the value property of the input elements
     var inputValue = inputElement.property("value");
-    console.log(inputValue);
+    var inputValue2 = inputElement2.property("value");
+    console.log(inputValue2);
     
     // Filter data where datetime equals user input 
     var filteredData = tableData.filter(record => record.datetime === inputValue);
@@ -43,3 +46,16 @@ function runEnter () {
     });
     
 };
+
+function BuildTable (data) {
+    tbody.html("");
+    data.forEach(function(element) {
+        var row = tbody.append("tr");
+        Object.entries(element).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td").text("");
+            var cell = row.append("td").text(value);
+        });
+});}
+
+BuildTable(tableData);
